ef6ce992c91e311ad125d0e8961ea5c7610555fa
Vanellope <vane11opeschw33tz@gmail.com>
2017-09-22 13:10:22 +0900

    BMsearch C implementation.

---
+++ BMsearch/BMsearch.c
@@ -0,0 +1,45 @@
+#include "stdio.h"
+#define max(a, b) ((a) > (b) ? a : b)
+
+int BMsearch(unsigned char *text, unsigned char *pattern);
+int length(unsigned char *text);
+int main(void);
+
+int main(void)
+{
+    unsigned char text[] = "112235553";
+    unsigned char pattern[] = "5553";
+    printf("%d", BMsearch(text, pattern));
+    return 0;
+}
+
+int BMsearch(unsigned char *text, unsigned char *pattern)
+{
+    int skip[256];
+    int i, j;
+    int textlen = length(text);
+    int patlen = length(pattern);
+    for (i = 0; i < 256; ++i) skip[i] = patlen;
+    for (i = 0; i < patlen - 1; ++i) skip[pattern[i]] = patlen - i - 1;
+    i = patlen - 1;
+    while (i < textlen) {
+        j = patlen - 1;
+        while (text[i] == pattern[j]) {
+            if (j == 0) return i;
+            --i, --j;
+        }
+        i = i + max(skip[text[i]], patlen - j);
+    }
+    return -1;
+}
+
+
+int length(unsigned char *text)
+{
+    int len = 0;
+    for(;*text != 0x00; ++text){
+        ++len;
+    }
+    printf("%d\n", len);
+    return len;
+}
